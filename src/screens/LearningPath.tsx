44b489104053ed0a670ed9bf1a40e234723fd835
José Núñez <79015124+josiext@users.noreply.github.com>
2022-07-01 12:23:13 -0400

    Refactor

--- src/screens/LearningPath.tsx
+++ src/screens/LearningPath.tsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
-import { StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+  Pressable,
+  Alert,
+  View,
+} from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
-import { Text, View } from "../components/Themed";
-import { RootTabScreenProps } from "../types";
+import { Text } from "../components/Themed";
+import { RootTabScreenProps } from "../../types";
 
 interface ILearningPath {
   id: string;
@@ -47,6 +54,7 @@ export default function LearningPath({
   const [groupOpened, setGroupOpened] = useState<typeof paths[0]["id"] | null>(
     null
   );
+  const [modalVisible, setModalVisible] = useState(false);
 
   const handleToggleTaskList = (id: string) => {
     if (groupOpened === id) setGroupOpened(null);
@@ -66,9 +74,34 @@ export default function LearningPath({
         ))}
       </View>
 
-      <TouchableOpacity style={{ padding: 10 }}>
+      <TouchableOpacity
+        style={{ padding: 10 }}
+        onPress={() => setModalVisible(true)}
+      >
         <Text>Create</Text>
       </TouchableOpacity>
+
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -82,6 +115,47 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
 
